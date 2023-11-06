3f2111363c7ec3f3e942fea43e2476d01fab7b51
Bhavi <nallabhavitran@gmail.com>
2023-11-06 18:14:15 +0530

    hi
    nothing

--- VT_codes/correlation_Checking.py
+++ VT_codes/correlation_Checking.py
@@ -5,8 +5,8 @@ import pandas as pd
 # n = int(input('Enter the length of the codeword: '))
 # length_info = n-1
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
 
 
 for n in range(5, 20):
@@ -15,10 +15,12 @@ for n in range(5, 20):
     Concatenate_kernel_code = []
     pob = []
     pob2 = []
-    length_info = n-1
-    for i in range(2 ** length_info):
+    length_info = n - 1
+    for i in range(2**length_info):
         val = list(np.binary_repr(i, length_info))
-        val = [int(x) for x in val]  # Converting the binary string to list into binary list
+        val = [
+            int(x) for x in val
+        ]  # Converting the binary string to list into binary list
         Binary_number.append(val)  # Creating a list of binary from length k
         Kc_b = kc.Kernel_codes()
         sep_values = Kc_b.kernel_code_encoder(val)
@@ -29,9 +31,18 @@ for n in range(5, 20):
 
     DNA_code = pd.DataFrame([Binary_number, Kernel_code, Concatenate_kernel_code, pob])
     DNA_code = DNA_code.transpose()
-    DNA_code.columns = ['Binary_number', 'Kernel_code', 'Concatenated kernel code', 'DNA_codeword']
-# print(DNA_code)
+    DNA_code.columns = [
+        "Binary_number",
+        "Kernel_code",
+        "Concatenated kernel code",
+        "DNA_codeword",
+    ]
+    # print(DNA_code)
 
-    print('Reverse_compliment_value of {} : {}'.format(n, kc.reverse_compliment_error(pob, pob2)))
+    print(
+        "Reverse_compliment_value of {} : {}".format(
+            n, kc.reverse_compliment_error(pob, pob2)
+        )
+    )
     mat_val = 2 * np.floor((n - 3) / 2)
-    print('the matching value: ', mat_val)
+    print("the matching value: ", mat_val)
